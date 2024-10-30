f5dd590a7627ae15c1d4cc768acae1e99fbbda82
Krzysztof Kulka <krzysztof.kulka1234@gmail.com>
2024-10-30 17:36:01 +0100

    succesful connection

--- connect_to_db.py
+++ connect_to_db.py
@@ -1,13 +1,16 @@
+from sqlite3 import connect
 from venv import create
 from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy import Column, Integer, String, create_engine
-URL="jdbc:postgresql://localhost:5432/postgres"
-engine=create_engine('sqlite:///example.db', echo=True)
+from dotenv import load_dotenv
+import os
+load_dotenv()
+URL = f"postgresql+psycopg2://postgres:{os.environ['POSTGRES_PSSWD']}@localhost:5432/postgres"
+engine=create_engine(URL, echo=True)
+connection=engine.connect()
 
-# Define the base model
 Base = declarative_base()
 
-# Define a sample model
 class User(Base):
     __tablename__ = 'users'
     
@@ -15,11 +18,8 @@ class User(Base):
     name = Column(String)
     age = Column(Integer)
 
-# Create the session factory
 Session = sessionmaker(bind=engine)
 session = Session()
-
-# Example of querying or adding data
 new_user = User(name="Alice", age=30)
 session.add(new_user)
 session.commit()

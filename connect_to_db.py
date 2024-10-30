d34db11a012a68970ae1af901b045b9593d62a4b
Krzysztof Kulka <krzysztof.kulka1234@gmail.com>
2024-10-30 17:41:29 +0100

    succesful connection

--- connect_to_db.py
+++ connect_to_db.py
@@ -1,25 +1,27 @@
+from enum import auto
 from sqlite3 import connect
 from venv import create
 from sqlalchemy.orm import sessionmaker, declarative_base
-from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy import Column, Integer, MetaData, String, create_engine, Table, insert
 from dotenv import load_dotenv
 import os
 load_dotenv()
 URL = f"postgresql+psycopg2://postgres:{os.environ['POSTGRES_PSSWD']}@localhost:5432/postgres"
 engine=create_engine(URL, echo=True)
 connection=engine.connect()
+metadata=MetaData()
 
-Base = declarative_base()
+passanger_table = Table('passanger', metadata, autoload_with=engine)
 
-class User(Base):
-    __tablename__ = 'users'
-    
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    age = Column(Integer)
+insert_stmt = insert(passanger_table).values(id_passanger=1, fk_user=1)
 
 Session = sessionmaker(bind=engine)
 session = Session()
-new_user = User(name="Alice", age=30)
-session.add(new_user)
-session.commit()
+with engine.connect() as conn:
+    conn.execute(insert_stmt)
+    conn.commit()
+
+# Querying data from the existing table
+result = session.query(passanger_table).all()
+for row in result:
+    print(row)

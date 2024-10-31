c919e7a94d8308f29ba31ae9906ff4bed36acc93
Krzysztof Kulka <krzysztof.kulka1234@gmail.com>
2024-10-31 15:40:15 +0100

    improved fk generatior

---
+++ editors.py
@@ -0,0 +1,24 @@
+from connect_to_db import execute_query, engine, metadata
+from sqlalchemy import Table, insert
+from faker import Faker
+import sys
+from fk_keys_hash_sets import load_set, user_fk_keys_set, save_set
+fake = Faker('pl_PL')
+editors_table = Table('editors', metadata, autoload_with=engine)
+
+def add_editor(foreign_key_user):
+    insert_stmt_ed = editors_table.insert().values(fk_user=foreign_key_user)
+    execute_query(insert_stmt_ed)
+
+def generate_editors(how_many):
+    load_set('user')
+    for _ in range(how_many):
+        f_key=fake.random_int(1, 1000)
+        while f_key not in user_fk_keys_set:
+            f_key=fake.random_int(1, 1000)
+        add_editor(f_key)
+        user_fk_keys_set.add(f_key)
+
+if __name__ == '__main__':
+    generate_editors(int(sys.argv[1]))
+    save_set('user')

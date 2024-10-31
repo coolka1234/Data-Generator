7d46f508e08aa396c7c61872ac953dcd625b90e8
Krzysztof Kulka <krzysztof.kulka1234@gmail.com>
2024-10-31 14:26:25 +0100

    working example created; now for individual cases

--- connect_to_db.py
+++ connect_to_db.py
@@ -1,9 +1,4 @@
-from enum import auto
-from math import e
-from sqlite3 import connect
-from venv import create
-from sqlalchemy.orm import sessionmaker, declarative_base
-from sqlalchemy import Column, Integer, MetaData, String, create_engine, Table, insert
+from sqlalchemy import MetaData, create_engine, Table, insert
 from dotenv import load_dotenv
 import os
 load_dotenv()
@@ -12,20 +7,11 @@ engine=create_engine(URL, echo=True)
 connection=engine.connect()
 metadata=MetaData()
 
-passenger_table = Table('passenger', metadata, autoload_with=engine)
-user_table=Table('app_user', metadata, autoload_with=engine)
-insert_stmt_user = insert(user_table).values(id_user=1, login="test", password="test123456", email="jerzy@gmail.com",
-                                                phone_number="123456789", name="Jerzy", surname="Kowalski")
-# insert_stmt_user = insert(passenger_table).values(id_passenger=1, fk_user=1)
+# user_table=Table('app_user', metadata, autoload_with=engine)
+# insert_stmt_user = insert(user_table).values(id_user=3, login="test23", password="test1234563", email="f2ranek@gmail.com",
+#                                              phone_number="123456783", name="Krycha", surname="Kowalski")
 
-
-Session = sessionmaker(bind=engine)
-session = Session()
-with engine.connect() as conn:
-    conn.execute(insert_stmt_user)
-    conn.commit()
-
-# Querying data from the existing table
-result = session.query(user_table).all()
-for row in result:
-    print(row)
+def execute_query(query):
+    with engine.connect() as conn:
+        conn.execute(query)
+        conn.commit()

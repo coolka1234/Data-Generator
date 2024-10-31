0735f721b50b9ded1c468636b822d07649e079ba
Krzysztof Kulka <krzysztof.kulka1234@gmail.com>
2024-10-31 16:03:42 +0100

    purchases somewhat work

---
+++ purchases.py
@@ -0,0 +1,17 @@
+from connect_to_db import execute_query, engine, metadata
+from sqlalchemy import Table, insert
+from faker import Faker
+import sys
+fake = Faker('pl_PL')
+purchases_table = Table('purchases', metadata, autoload_with=engine)
+
+def add_purchase(date, amount):
+    insert_stmt_purchase = purchases_table.insert().values(date=date, amount=amount)
+    execute_query(insert_stmt_purchase)
+
+def generate_purchases(how_many):
+    for _ in range(how_many):
+        add_purchase(fake.date_time_this_year(), abs(fake.pyfloat(2, 2)))
+
+if __name__ == '__main__':
+    generate_purchases(int(sys.argv[1]))

1e81edd34d62ac7d6dff284fd4a2210fbaa5d39e
Krzysztof Kulka <krzysztof.kulka1234@gmail.com>
2024-10-31 16:19:43 +0100

    adding vehicles

--- vehicle.py
+++ vehicle.py
@@ -5,13 +5,13 @@ import sys
 fake = Faker('pl_PL')
 vehicles_table = Table('vehicles', metadata, autoload_with=engine)
 
-def add_purchase(date, amount):
-    insert_stmt_purchase = vehicles_table.insert().values(date=date, amount=amount)
-    execute_query(insert_stmt_purchase)
+def add_vehicle(date, amount):
+    insert_stmt_vehicle = vehicles_table.insert().values(date=date, amount=amount)
+    execute_query(insert_stmt_vehicle)
 
 def generate_purchases(how_many):
     for _ in range(how_many):
-        add_purchase(fake.date_time_this_year(), abs(fake.pyfloat(2, 2)))
+        add_vehicle(fake.date_time_this_year(), abs(fake.pyfloat(2, 2)))
 
 if __name__ == '__main__':
     generate_purchases(int(sys.argv[1]))
